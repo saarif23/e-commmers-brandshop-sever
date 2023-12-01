29dd7c9a3896a9dcb24439d28c6cd92ccba6b2c2
saarif23 <arif23s.001@gmail.com>
2023-12-01 10:42:14 +0600

    added update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 //Port 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 7000
 
 //MIDDLEWARE
 app.use(cors());
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         const productCollection = client.db("productsDB").collection("products")
         const myCartCollection = client.db("myCartDB").collection("mycart")
 
@@ -103,7 +103,7 @@ async function run() {
         //my cart delete one
         app.delete('/mycart/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: id };
+            const query = { _id: new ObjectId (id) };
             const result = await myCartCollection.deleteOne(query);
             res.send(result)
         })
@@ -113,7 +113,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
